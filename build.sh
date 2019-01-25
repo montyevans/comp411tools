00a2b3625ed5e3930d5af3faf1bd250d630bcbcd
montyevans <montyevans@gmail.com>
2019-01-25 15:20:17 -0500

    Commenting.

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-for i in {1..5}; do gcc ex5.c -o ex5; done
+# I wrote this line about eight times before I gave up and shoved it into a script.
+# 
+# TO RUN: "./build.sh" - just like a C executable.
+#
+# Hope it's handy.
+# M
+
+for i in {1..5}; do gcc ex$i.c -o ex$i; done
--- test-script.py
+++ test-script.py
@@ -1,3 +1,21 @@
+'''
+A script to automate the somewhat tedious process of running the assignment executables
+against all the test input files and diffing against the expected output files.
+
+It needs to be run from the directory containing both the executables and the testIO directory,
+or it will collapse and fail miserably.
+
+The script will show all the output diffs. If all is well, it will produce exactly nothing.
+Otherwise, you'll see the line-by-line differences for your fixing pleasure, as usual.
+
+ 
+TO RUN: "python test-script.py", then cross your fingers.
+
+
+Hope it's handy.
+M
+'''
+
 import os
 import subprocess
 
@@ -15,6 +33,8 @@ for f in infiles:
 
     infile = open(ioDir + "/" + f)
 
+    # I couldn't figure out piping with the subprocess module, so the script stores the output in a temporary file,
+    # uses a second subprocess to diff that with the expected-output file, and then removes the temp file.
     tmp = open("tmp", "w+")
 
     subprocess.call(["./" + executable], stdin=infile, stdout=tmp)
